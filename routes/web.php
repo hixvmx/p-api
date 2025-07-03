a7504799630ab3048df7dc22c8385e6ba3d57ac8
hixvm <hixvmx@gmail.com>
2025-07-03 16:07:20 +0100

    redirect undefined routes to welcome page

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// undefined routes
+Route::fallback(function () {
+    return view('welcome');
+});
